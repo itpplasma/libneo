4463d2ccc6a1e6e81704a5d9e231d392018be641
Rico Buchholz <buchholz@tugraz.at>
2019-03-01 12:09:52 +0100

    Clean up comments.
    Should be used for explaining why the code looks like it does, not for
    version control.

--- rk45/odeint_allroutines.f90
+++ rk45/odeint_allroutines.f90
@@ -26,15 +26,10 @@ contains
     hmin=0.d0
 
     call odeint(y,nvar,x1,x2,eps,h1,hmin,nok,nbad,derivs,rkqs)
-! 17.07.2016      call RK4b(y,nvar,x1,h1,derivs)
-!
+
     return
   END SUBROUTINE odeint_allroutines
-!
-!------------------------------------------------------------------------------
 
-!
-!------------------------------------------------------------------------------
   SUBROUTINE alloc_odeint(nvar)
     use odeint_mod
 
@@ -49,29 +44,28 @@ contains
       deallocate(ak2,ak3,ak4,ak5,ak6,ytemp)
       deallocate(yerr,ytemp1)
     end if
-!
+
   END SUBROUTINE alloc_odeint
-!------------------------------------------------------------------------------
-!
+
   SUBROUTINE odeint(ystart,nvar,x1,x2,eps,h1,hmin,nok,nbad,derivs,rkqs)
-!
+
     use odeint_mod, only : kmax,kount,ialloc,dxsav,dydx,xp,y,yscal,yp
-!
-! 18.07.2016
+
+
     use gbpi_mod, only : ierrfield
-! 18.07.2016 end
+
     implicit double precision (a-h,o-z)
-!
+
     INTEGER nbad,nok,nvar,KMAXX,MAXSTP
     double precision eps,h1,hmin,x1,x2,ystart(nvar),TINY
     EXTERNAL derivs,rkqs
     PARAMETER (MAXSTP=1000000,TINY=1.e-30)
     INTEGER i,nstp
     double precision h,hdid,hnext,x,xsav
-!
+
     ialloc=1
     call alloc_odeint(nvar)
-!
+
     x=x1
     h=sign(h1,x2-x1)
     nok=0
@@ -85,14 +79,14 @@ contains
 
     do 16 nstp=1,MAXSTP
       call derivs(x,y,dydx)
-! 18.07.2016
+
       if (ierrfield.ne.0) then
         print*,ierrfield,'=ierrfield, derivs, odeint'
         ialloc=0
         call alloc_odeint(nvar)
         return
       end if
-! 18.07.2016 end
+
       do 12 i=1,nvar
         yscal(i)=abs(y(i))+abs(h*dydx(i))+TINY
 12    continue
@@ -110,14 +104,14 @@ contains
       end if
       if ((x+h-x2)*(x+h-x1).gt.0.) h=x2-x
       call rkqs(y,dydx,nvar,x,h,eps,yscal,hdid,hnext,derivs)
-! 18.07.2016
+
       if (ierrfield.ne.0) then
         print*,ierrfield,'=ierrfield, rkqs, odeint'
         ialloc=0
         call alloc_odeint(nvar)
         return
       end if
-! 18.07.2016 end
+
       if (hdid.eq.h) then
         nok=nok+1
       else
@@ -138,37 +132,34 @@ contains
         call alloc_odeint(nvar)
         return
       end if
-!        if(abs(hnext).lt.hmin) pause
-!     *'stepsize smaller than minimum in odeint'
-! 17.07.2016
+
       if (abs(hnext).lt.hmin) then
         print*,'stepsize smaller than minimum in odeint'
         write(99,*)'stepsize smaller than minimum in odeint, stop'
         stop
       end if
-! 17.07.2016 end
+
       h=hnext
 16  continue
-! 17.07.2016      pause 'too many steps in odeint'
+
     print*,'too many steps in odeint'
     write(99,*)'to many steps in odeint, stop'
     stop
-! 17.07.2016 end
+
 
     ialloc=0
     call alloc_odeint(nvar)
     return
   END subroutine odeint
-!cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
   SUBROUTINE rkck(y,dydx,n,x,h,yout,yerr,derivs)
-!
+
     use odeint_mod, only : ak2,ak3,ak4,ak5,ak6,ytemp
-! 18.07.2016
+
     use gbpi_mod
-! 18.07.2016 end
-!
+
     implicit double precision (a-h,o-z)
-!
+
     INTEGER n
     double precision h,x,dydx(n),y(n),yerr(n),yout(n)
     EXTERNAL derivs
@@ -193,37 +184,37 @@ contains
       ytemp(i)=y(i)+B21*h*dydx(i)
 11  continue
     call derivs(x+A2*h,ytemp,ak2)
-! 18.07.2016
+
     if (ierrfield.ne.0) return
-! 18.07.2016 end
+
     do 12 i=1,n
       ytemp(i)=y(i)+h*(B31*dydx(i)+B32*ak2(i))
 12  continue
     call derivs(x+A3*h,ytemp,ak3)
-! 18.07.2016
+
     if (ierrfield.ne.0) return
-! 18.07.2016 end
+
     do 13 i=1,n
       ytemp(i)=y(i)+h*(B41*dydx(i)+B42*ak2(i)+B43*ak3(i))
 13  continue
     call derivs(x+A4*h,ytemp,ak4)
-! 18.07.2016
+
     if (ierrfield.ne.0) return
-! 18.07.2016 end
+
     do 14 i=1,n
       ytemp(i)=y(i)+h*(B51*dydx(i)+B52*ak2(i)+B53*ak3(i)+B54*ak4(i))
 14  continue
     call derivs(x+A5*h,ytemp,ak5)
-! 18.07.2016
+
     if (ierrfield.ne.0) return
-! 18.07.2016 end
+
     do 15 i=1,n
       ytemp(i)=y(i)+h*(B61*dydx(i)+B62*ak2(i)+B63*ak3(i)+B64*ak4(i)+B65*ak5(i))
 15  continue
     call derivs(x+A6*h,ytemp,ak6)
-! 18.07.2016
+
     if (ierrfield.ne.0) return
-! 18.07.2016 end
+
     do 16 i=1,n
       yout(i)=y(i)+h*(C1*dydx(i)+C3*ak3(i)+C4*ak4(i)+C6*ak6(i))
 16  continue
@@ -232,13 +223,13 @@ contains
 17  continue
     return
   END subroutine
-!cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
   SUBROUTINE rkqs(y,dydx,n,x,htry,eps,yscal,hdid,hnext,derivs)
-!
+
     use odeint_mod, only : yerr,ytemp1
-!
+
     implicit double precision (a-h,o-z)
-!
+
     INTEGER n
     double precision eps,hdid,hnext,htry,x,dydx(n),y(n),yscal(n)
     EXTERNAL derivs
@@ -257,12 +248,12 @@ contains
       htemp=SAFETY*h*(errmax**PSHRNK)
       h=sign(max(abs(htemp),0.1*abs(h)),h)
       xnew=x+h
-!      if(xnew.eq.x) pause 'stepsize underflow in rkqs'
+
       if (xnew.eq.x) then
         print *,'stepsize underflow in rkqs, x,y = ',x,y
-! 17.02.2016
+
         write(99,*)'stepsize underflow in rkqs, x,y = ',x,y
-! 17.02.2016 end
+
         stop
       end if
       goto 1
@@ -280,52 +271,52 @@ contains
       return
     end if
   END subroutine rkqs
-!cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-! 28.03.2016
+
+
   SUBROUTINE RK4b(Y,N,X,H,DERIVS)
-!
+
     use gbpi_mod
-!
+
     implicit double precision (a-h,o-z)
     external derivs
     PARAMETER (NMAX=12)
     DIMENSION Y(N),DYDX(NMAX),YT(NMAX),DYT(NMAX),DYM(NMAX)
-! 22.08.2011      HH=H*0.5
+
     HH=H*0.5d0
-! 22.08.2011      H6=H/6.
+
     H6=H/6.0d0
     XH=X+HH
-!
+
     CALL DERIVS(X,Y,DYDX)
     if(ierrfield.ne.0) return
-!
+
     DO 11 I=1,N
       YT(I)=Y(I)+HH*DYDX(I)
 11  CONTINUE
-!
+
     CALL DERIVS(XH,YT,DYT)
     if (ierrfield.ne.0) return
-!
+
     DO 12 I=1,N
       YT(I)=Y(I)+HH*DYT(I)
 12  CONTINUE
-!
+
     CALL DERIVS(XH,YT,DYM)
     if (ierrfield.ne.0) return
-!
+
     DO 13 I=1,N
       YT(I)=Y(I)+H*DYM(I)
       DYM(I)=DYT(I)+DYM(I)
 13  CONTINUE
-!
+
     CALL DERIVS(X+H,YT,DYT)
     if (ierrfield.ne.0) return
-!
+
     DO 14 I=1,N
       Y(I)=Y(I)+H6*(DYDX(I)+DYT(I)+2.d0*DYM(I))
 14  CONTINUE
-! 24.08.2011                         X=X+H
+
     RETURN
   END subroutine RK4b
-! 28.03.2016
+
 end module odeint_mod
