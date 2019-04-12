cea3174d70dcf87615187febd07e20468f300cf6
Rico Buchholz <buchholz@tugraz.at>
2019-04-12 13:38:10 +0200

    Add test to execute test_arnoldi.x.
    For this purpose a wrapper shell script has been writen for two reasons.
    For one, fortran programs do not seem to return a value different from
    zero when encountering a 'stop', while a nonzero return value on error
    is expected by ctest. Second, the python script test_arnoldi.py needs to
    be executed, before test_arnoldi.x can be run, as it will create the
    required references.
    The first can and is circumvented with setting the property
    FAIL_REGULAR_EXPRESSION (looking for 'STOP');
    
    Note: the wrapper might be unnecessary, as test_arnoldi.py &&
    test_arnoldi.x might do the job.

---
+++ tests/shell_scripts/test_wrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Short wrapper for the test functions.
+# This is required, as an abort with 'stop' is still counted as sucess
+# by ctest.
+#
+# Usage:
+#   ./test_wrapper.sh test_to_execute
+#
+# Where 'test_to_execute' is the name of an executable.
+
+test_to_execute=$1
+
+if [ "x$2" == "xyes" ] ; then
+  echo "running test_arnoldi.py"
+  pwd
+  ../tests/pythons_scripts/test_arnoldi.py
+fi
+
+./$test_to_execute | grep -i " STOP "
+if [ "x$?" == "x0" ] ; then
+  exit 1
+else
+  exit 0
+fi
