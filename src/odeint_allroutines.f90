4894e1e4c6a7ac614929ce81e733c1db1943d112
Rico Buchholz <buchholz@tugraz.at>
2019-10-14 10:00:05 +0200

    Second part of odeint cleanup.
    Hopefully the last (except for a renaming of the file).

--- src/odeint_allroutines.f90
+++ src/odeint_allroutines.f90
@@ -88,92 +88,83 @@ contains
     real(kind=real_kind), intent(in) :: x1, x2, eps, h1, hmin
     real(kind=real_kind), intent(inout) :: ystart(nvar)
 
-    integer :: KMAXX
     integer :: i,nstp
     integer :: ierr
 
-    real(kind=real_kind), parameter :: TINY_VALUE=1.e-30
     real(kind=real_kind) :: h,hdid,hnext,x,xsav
 
-    ialloc=1
+    xsav = 1.234e5
+
+    ialloc = 1
     call alloc_odeint(nvar)
 
-    x=x1
-    h=sign(h1,x2-x1)
-    nok=0
-    nbad=0
-    kount=0
+    x = x1
+    h = sign(h1,x2-x1)
+    nok = 0
+    nbad = 0
+    kount = 0
+
     do i=1,nvar
-      y(i)=ystart(i)
+      y(i) = ystart(i)
     end do
 
-    if (kmax.gt.0) xsav=x-2.*dxsav
+    if (kmax .gt. 0) xsav = x-2.*dxsav
 
     do nstp=1,MAXSTP
-      call derivs(x,y,dydx,ierr)
+      call derivs(x, y, dydx, ierr)
       if (ierr.ne.0) then
-        print*,ierr,'=ierrfield, derivs, odeint'
-        ialloc=0
+        write(*,*) ierr,'=ierrfield, derivs, odeint'
+        ialloc = 0
         call alloc_odeint(nvar)
         return
       end if
 
       do i=1,nvar
-        yscal(i)=abs(y(i))+abs(h*dydx(i))+TINY_VALUE
+        yscal(i) = abs(y(i)) + abs(h*dydx(i)) + TINY_LOCAL
       end do
-      if (kmax.gt.0) then
-        if (abs(x-xsav).gt.abs(dxsav)) then
-          if (kount.lt.kmax-1) then
-            kount=kount+1
-            xp(kount)=x
+      if (kmax .gt. 0) then
+        if (abs(x-xsav) .gt. abs(dxsav)) then
+          if (kount .lt. kmax-1) then
+            kount = kount+1
+            xp(kount) = x
             do i=1,nvar
-              yp(i,kount)=y(i)
+              yp(i,kount) = y(i)
             end do
-            xsav=x
+            xsav = x
           end if
         end if
       end if
-      if ((x+h-x2)*(x+h-x1).gt.0.) h=x2-x
+      if ((x+h-x2)*(x+h-x1) .gt. 0.0) h=x2-x
       call rkqs(y,dydx,nvar,x,h,eps,yscal,hdid,hnext,derivs)
 
-      if (hdid.eq.h) then
-        nok=nok+1
+      if (hdid .eq. h) then
+        nok = nok+1
       else
-        nbad=nbad+1
+        nbad = nbad+1
       end if
-      if ((x-x2)*(x2-x1).ge.0.) then
+      if ((x-x2)*(x2-x1) .ge. 0.0) then
         do i=1,nvar
-          ystart(i)=y(i)
+          ystart(i) = y(i)
         end do
-        if (kmax.ne.0) then
-          kount=kount+1
-          xp(kount)=x
+        if (kmax .ne. 0) then
+          kount = kount+1
+          xp(kount) = x
           do i=1,nvar
-            yp(i,kount)=y(i)
+            yp(i,kount) = y(i)
           end do
         end if
-        ialloc=0
+        ialloc = 0
         call alloc_odeint(nvar)
         return
       end if
 
-      if (abs(hnext).lt.hmin) then
-        print*,'stepsize smaller than minimum in odeint'
-        write(99,*)'stepsize smaller than minimum in odeint, stop'
-        stop
-      end if
-
+      if (abs(hnext) .lt. hmin) write(*,*) 'stepsize smaller than minimum in odeint'
       h=hnext
     end do
 
-    print*,'too many steps in odeint'
-    write(99,*)'to many steps in odeint, stop'
-    stop
-
-
+    write(*,*) 'too many steps in odeint'
     ialloc=0
     call alloc_odeint(nvar)
-    return
   end subroutine odeint
 
   subroutine rkck(y, dydx, n, x, h, yout, yerr, derivs)
@@ -184,54 +175,61 @@ contains
 
     procedure(compute_derivative) :: derivs
 
-    integer :: i, n, ierr
-    real(kind=real_kind), parameter :: A2=0.2d0, A3=0.3d0, A4=0.6d0, &
-      & A5=1.d0, A6=0.875d0, &
-      & B21=0.2d0, B31=3.d0/40.d0, B32=9.d0/40.d0, B41=0.3d0, B42=-0.9d0, &
-      & B43=1.2d0, B51=-11.d0/54.d0, B52=2.5d0, B53=-70.d0/27.d0, &
-      & B54=35.d0/27.d0, B61=1631.d0/55296.d0, B62=175.d0/512.d0, &
-      & B63=575.d0/13824.d0, B64=44275.d0/110592.d0, B65=253.d0/4096.d0, &
-      & C1=37.d0/378.d0, C3=250.d0/621.d0, C4=125.d0/594.d0, &
-      & C6=512.0d0/1771.0d0, &
-      & DC1=C1-2825.d0/27648.d0, DC3=C3-18575.d0/48384.d0, &
-      & DC4=C4-13525.d0/55296.d0, DC5=-277.d0/14336.d0, DC6=C6-.25d0
-    real(kind=real_kind) :: h,x,dydx(n),y(n),yerr(n),yout(n)
+    integer, intent(in) :: n
+    real(kind=real_kind), intent(in) :: y(n), dydx(n), x, h
+    real(kind=real_kind), intent(out) :: yout(n), yerr(n)
+
+    integer :: i, ierr
+    real(kind=real_kind), parameter :: A2 = 0.2d0, A3 = 0.3d0, A4 = 0.6d0, &
+      & A5 = 1.0d0, A6 = 0.875d0, &
+      & B21 = 0.2d0, B31 = 3.0d0/40.0d0, B32 = 9.0d0/40.0d0, B41 = 0.3d0, B42 = -0.9d0, &
+      & B43 = 1.2d0, B51 = -11.0d0/54.0d0, B52 = 2.5d0, B53 = -70.0d0/27.0d0, &
+      & B54 = 35.0d0/27.0d0, B61 = 1631.0d0/55296.0d0, B62 = 175.0d0/512.0d0, &
+      & B63 = 575.0d0/13824.0d0, B64 = 44275.0d0/110592.0d0, B65 = 253.0d0/4096.0d0, &
+      & C1 = 37.0d0/378.0d0, C3 = 250.0d0/621.0d0, C4 = 125.0d0/594.0d0, &
+      & C6 = 512.0d0/1771.0d0, &
+      & DC1 = C1-2825.0d0/27648.0d0, DC3 = C3-18575.0d0/48384.0d0, &
+      & DC4 = C4-13525.0d0/55296.0d0, DC5 = -277.0d0/14336.0d0, DC6 = C6-0.25d0
 
     do i=1,n
-      ytemp(i)=y(i)+B21*h*dydx(i)
+      ytemp(i) = y(i) + B21*h*dydx(i)
     end do
-    call derivs(x+A2*h,ytemp,ak2, ierr)
-    if (ierr.ne.0) return
+
+    call derivs(x+A2*h, ytemp, ak2, ierr)
+    if (ierr .ne. 0) return
 
     do i=1,n
-      ytemp(i)=y(i)+h*(B31*dydx(i)+B32*ak2(i))
+      ytemp(i) = y(i) + h*(B31*dydx(i) + B32*ak2(i))
     end do
-    call derivs(x+A3*h,ytemp,ak3,ierr)
-    if (ierr.ne.0) return
+
+    call derivs(x+A3*h, ytemp, ak3, ierr)
+    if (ierr .ne. 0) return
 
     do i=1,n
-      ytemp(i)=y(i)+h*(B41*dydx(i)+B42*ak2(i)+B43*ak3(i))
+      ytemp(i) = y(i) + h*(B41*dydx(i) + B42*ak2(i) + B43*ak3(i))
     end do
-    call derivs(x+A4*h,ytemp,ak4,ierr)
-    if (ierr.ne.0) return
+    call derivs(x+A4*h, ytemp, ak4, ierr)
+    if (ierr .ne. 0) return
 
     do i=1,n
-      ytemp(i)=y(i)+h*(B51*dydx(i)+B52*ak2(i)+B53*ak3(i)+B54*ak4(i))
+      ytemp(i) = y(i) + h*(B51*dydx(i) + B52*ak2(i) + B53*ak3(i) + B54*ak4(i))
     end do
-    call derivs(x+A5*h,ytemp,ak5,ierr)
-    if (ierr.ne.0) return
+    call derivs(x+A5*h, ytemp, ak5, ierr)
+    if (ierr .ne. 0) return
 
     do i=1,n
-      ytemp(i)=y(i)+h*(B61*dydx(i)+B62*ak2(i)+B63*ak3(i)+B64*ak4(i)+B65*ak5(i))
+      ytemp(i) = y(i) + h*(B61*dydx(i) + B62*ak2(i) + B63*ak3(i) &
+        & + B64*ak4(i) + B65*ak5(i))
     end do
-    call derivs(x+A6*h,ytemp,ak6,ierr)
-    if (ierr.ne.0) return
+    call derivs(x+A6*h, ytemp, ak6, ierr)
+    if (ierr .ne. 0) return
 
     do i=1,n
-      yout(i)=y(i)+h*(C1*dydx(i)+C3*ak3(i)+C4*ak4(i)+C6*ak6(i))
+      yout(i) = y(i) + h*(C1*dydx(i) + C3*ak3(i) + C4*ak4(i) + C6*ak6(i))
     end do
     do i=1,n
-      yerr(i)=h*(DC1*dydx(i)+DC3*ak3(i)+DC4*ak4(i)+DC5*ak5(i)+DC6*ak6(i))
+      yerr(i) = h*(DC1*dydx(i) + DC3*ak3(i) + DC4*ak4(i) + DC5*ak5(i) &
+        & + DC6*ak6(i))
     end do
 
   end subroutine rkck
@@ -243,48 +241,54 @@ contains
 
     procedure(compute_derivative) :: derivs
 
-    integer :: i, n
-    real(kind=real_kind), parameter :: SAFETY=0.9d0,PGROW=-.2d0, &
-      & PSHRNK=-.25d0,  ERRCON=1.89d-4
-    real(kind=real_kind) :: eps,hdid,hnext,htry,x,dydx(n),y(n),yscal(n)
-    real(kind=real_kind) :: errmax,h,htemp,xnew
+    integer, intent(in) :: n
+    real(kind=real_kind), intent(in) :: dydx(n), htry, eps, yscal(n)
+    real(kind=real_kind), intent(inout) :: y(n), x
+    real(kind=real_kind), intent(out) :: hdid, hnext
 
-    h=htry
-1   call rkck(y,dydx,n,x,h,ytemp1,yerr,derivs)
-    errmax=0.
-    do i=1,n
-      errmax=max(errmax,abs(yerr(i)/yscal(i)))
-    end do
-    errmax=errmax/eps
-    if (errmax.gt.1.) then
-      htemp=SAFETY*h*(errmax**PSHRNK)
-      h=sign(max(abs(htemp),0.1*abs(h)),h)
-      xnew=x+h
+    integer :: i
+
+    real(kind=real_kind) :: errmax, h, htemp, xnew
+
+    h = htry
+
+    do
+
+      call rkck(y, dydx, n, x, h, ytemp1, yerr, derivs)
+
+      errmax=0.0
+      do i=1,n
+        errmax = max(errmax, abs(yerr(i)/yscal(i)))
+      end do
+      errmax=errmax/eps
+
+      if (errmax .le. 1.0) exit
+
+      htemp = SAFETY*h*(errmax**PSHRNK)
+      h = sign(max(abs(htemp),0.1*abs(h)),h)
+      xnew = x+h
 
       if (xnew.eq.x) then
-        print *,'stepsize underflow in rkqs, x,y = ',x,y
-
-        write(99,*)'stepsize underflow in rkqs, x,y = ',x,y
-
+        print *,'stepsize underflow in rkqs, x,y = ', x, y
         stop
       end if
-      goto 1
+
+    end do
+
+    if (errmax .gt. ERRCON) then
+      hnext = SAFETY*h*(errmax**PGROW)
     else
-      if(errmax.gt.ERRCON)then
-        hnext=SAFETY*h*(errmax**PGROW)
-      else
-        hnext=5.*h
-      endif
-      hdid=h
-      x=x+h
-      do i=1,n
-        y(i)=ytemp1(i)
-      end do
-      return
+      hnext = 5.0*h
     end if
+    hdid = h
+    x = x+h
+    do i=1,n
+      y(i)=ytemp1(i)
+    end do
+
   end subroutine rkqs
 
-  subroutine RK4b(Y,N,X,H,DERIVS)
+  subroutine rk4b(y, n, x, h, derivs)
 
     use libneo_kinds, only : real_kind
 
@@ -292,49 +296,49 @@ contains
 
     procedure(compute_derivative) :: derivs
 
-    integer, intent(in) :: N
-    real(kind=real_kind), intent(in) :: X, H
-    real(kind=real_kind), intent(inout) :: Y(N)
+    integer, intent(in) :: n
+    real(kind=real_kind), intent(in) :: x, h
+    real(kind=real_kind), intent(inout) :: y(n)
 
     integer, parameter :: NMAX=12
     integer :: i, ierr
-    real(kind=real_kind) :: DYDX(NMAX), YT(NMAX), DYT(NMAX), DYM(NMAX)
-    real(kind=real_kind) :: XH, HH, H6
+    real(kind=real_kind) :: dydx(NMAX), yt(NMAX), dyt(NMAX), dym(NMAX)
+    real(kind=real_kind) :: xh, hh, h6
 
-    HH=H*0.5d0
+    hh = h*0.5d0
 
-    H6=H/6.0d0
-    XH=X+HH
+    h6 = h/6.0d0
+    xh = x + hh
 
-    call DERIVS(X,Y,DYDX, ierr)
-    if (ierr.ne.0) return
+    call derivs(x, y, dydx, ierr)
+    if (ierr .ne. 0) return
 
-    do I=1,N
-      YT(I)=Y(I)+HH*DYDX(I)
+    do i=1,n
+      yt(i) = y(i) + hh*dydx(i)
     end do
 
-    call DERIVS(XH,YT,DYT, ierr)
-    if (ierr.ne.0) return
+    call derivs(xh, yt, dyt, ierr)
+    if (ierr .ne. 0) return
 
-    do I=1,N
-      YT(I)=Y(I)+HH*DYT(I)
+    do i=1,n
+      yt(i) = y(i) + hh*dyt(i)
     end do
 
-    call DERIVS(XH,YT,DYM, ierr)
-    if (ierr.ne.0) return
+    call derivs(xh, yt, dym, ierr)
+    if (ierr .ne. 0) return
 
-    do I=1,N
-      YT(I)=Y(I)+H*DYM(I)
-      DYM(I)=DYT(I)+DYM(I)
+    do i=1,n
+      yt(i) = y(i) + h*dym(i)
+      dym(i)= dyt(i) + dym(i)
     end do
 
-    call DERIVS(X+H,YT,DYT, ierr)
-    if (ierr.ne.0) return
+    call derivs(x+h, yt, dyt, ierr)
+    if (ierr .ne. 0) return
 
-    do I=1,N
-      Y(I)=Y(I)+H6*(DYDX(I)+DYT(I)+2.d0*DYM(I))
+    do i=1,n
+      y(i) = y(i) + h6*(dydx(i) + dyt(i) + 2.0d0*dym(i))
     end do
 
-  end subroutine RK4b
+  end subroutine rk4b
 
 end module ode_integration
