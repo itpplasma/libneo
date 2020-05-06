86fe3021ede03ff912d137403959b3c16e39fb72
Philipp Ulbl <philipp.ulbl@student.tugraz.at>
2020-05-06 10:17:53 +0200

    Added multiple shot runs.

--- matlab/GPEC_interface/script_GPEC.m
+++ matlab/GPEC_interface/script_GPEC.m
@@ -9,17 +9,38 @@
 %author:   Philipp Ulbl
 %created:  01.04.2020
 
-studyname = 'test';
+studyname = 'BifurcThres';
 
-shot = 33120;
-time = 5500;
+%Runs to make
+run = [30835, 2300;...
+       30835, 3000;...
+       33120, 5500;...
+       33120, 5635;...
+       33133, 2600;...
+       33133, 3000;...
+       33353, 2325;...
+       33353, 2670;...
+       33353, 2900];
 
-runpath = ['/temp/ulbl_p/GPEC/', studyname, '/', num2str(shot), '_', num2str(time),'/'];
-gfile = ['/temp/ulbl_p/AUG/SHOTS/',num2str(shot),'/g',num2str(shot),'.',num2str(time),'_EQH'];
-cfile = ['/temp/ulbl_p/AUG/SHOTS/',num2str(shot),'/',num2str(shot),'.',num2str(time), '_coil.dat'];
+for k = 1:size(run, 1)
 
-gpec = GPEC_interface(runpath, shot, time, studyname);
-gpec.setEqui(gfile);
-gpec.setCoil(cfile);
-gpec.write();
-gpec.run();
+    shot = run(k, 1);
+    time = run(k, 2);
+
+    runpath = ['/temp/ulbl_p/GPEC/', studyname, '/', num2str(shot), '_', num2str(time),'/'];
+    gfile = ['/temp/ulbl_p/AUG/SHOTS/',num2str(shot),'/g',num2str(shot),'.',num2str(time),'_EQH'];
+    cfile = ['/temp/ulbl_p/AUG/SHOTS/',num2str(shot),'/',num2str(shot),'.',num2str(time), '_coil.dat'];
+
+    gpec = GPEC_interface(runpath, shot, time, studyname);
+    gpec.setEqui(gfile);
+    gpec.setCoil(cfile);
+    gpec.write();
+    
+    try
+        gpec.run();
+    catch
+        warning('error in GPEC occurred.')
+        gpec.run();
+    end
+        
+end
