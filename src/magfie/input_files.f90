529a212cf6ecbf6dc64d82a60bbcce04d0e3def3
Rico Buchholz <buchholz@tugraz.at>
2020-09-11 14:15:59 +0200

    Add variable for equilibrium file type.
    Needed by neo-2.

--- src/magfie/input_files.f90
+++ src/magfie/input_files.f90
@@ -1,7 +1,8 @@
 module input_files
   character(len=1024) :: eqfile, cfile, gfile,pfile,convexfile,fluxdatapath
   integer :: iunit=1738
-!
+  integer :: ieqfile=1 ! equilibrium file type (0 - old, 1 - EFIT)
+
   data eqfile  /'d3d.equ'/
   data cfile   /'DATA/ccoil.dat'/
 !  data gfile   /'gfiles/shot115452/g115452.03525'/
