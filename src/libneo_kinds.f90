e45e10df524430622c2ff150f523ad318317332a
Rico Buchholz <buchholz@tugraz.at>
2021-10-29 17:54:20 +0200

    Add kind parameter for integers.
    Not sure about the name, and added a todo for this.

--- src/libneo_kinds.f90
+++ src/libneo_kinds.f90
@@ -4,7 +4,16 @@
 !> library and can be used at code interfacing with libneo.
 !>
 !> In most cases the *_kind parameters are the ones that should be used.
+!>
+!> \todo Name of the interger kinds? I think the b comes from byte but
+!>   does the byte count need to match the indicated value?
 module libneo_kinds
+  ! integer kinds
+  integer, parameter :: i4b = selected_int_kind(9)
+  integer, parameter :: i2b = selected_int_kind(4)
+  integer, parameter :: i1b = selected_int_kind(2)
+  integer, parameter :: integer_kind=i4b
+
   ! real kinds
   integer, parameter :: rp = kind(1.0)
   integer, parameter :: dp = kind(1.0d0)
