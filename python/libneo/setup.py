d0713c8538c57ca7ef130191b93461cbc69d76e1
Christopher <albert@alumni.tugraz.at>
2023-05-30 18:13:28 +0200

    Added more stuff from SIMPLE

--- python/libneo/setup.py
+++ python/libneo/setup.py
@@ -11,7 +11,7 @@ setup(
         Extension('libneo.magfie',
                   sources=['f2py_magfie.f90',
                            '../../src/magfie/magfie_vmec.f90',
-                           '../../src/magfie/spline_vmec_data.f90'],
+                           '../../src/spline_vmec_data.f90'],
                   include_dirs=['../../build'],
                   libraries=['libneo'],
                   library_dirs=['../../build/'])
