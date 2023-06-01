98c59046e077a145172c06309a455d2b47a2585a
Christopher <albert@alumni.tugraz.at>
2023-06-01 04:25:08 +0200

    Python package init

---
+++ python/f2py_interfaces/libneo/__init__.py
@@ -0,0 +1,13 @@
+"""
+libneo
+-----------
+
+This is the package containing the modules offered by the libneo
+Python interface. Currently this is only the magfie module with support for
+VMEC equilibrium fields. The magfie module is directly generated via f2py
+on package install.
+
+For installation instructions and more details, please refer to the README.md file.
+"""
+
+__all__ = ['magfie']
