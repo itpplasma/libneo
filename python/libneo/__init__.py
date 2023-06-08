e81ab1fd18ca42ac2ce926c90cb08a75d4491c71
Christopher <albert@alumni.tugraz.at>
2023-06-08 13:45:33 +0200

    Move eqdsk.py to library

--- python/libneo/__init__.py
+++ python/libneo/__init__.py
@@ -3,11 +3,12 @@ libneo
 -----------
 
 This is the package containing the modules offered by the libneo
-Python interface. Currently this is only the magfie module with support for
+Python interface. Currently this is mainly the magfie module with support for
 VMEC equilibrium fields. The magfie module is directly generated via f2py
 on package install.
 """
 
 from .boozer import *
+from .eqdsk import *
 from .convert_neo_in_to_fortran_namelist import *
 from .getHeaderDataVMEC import *
