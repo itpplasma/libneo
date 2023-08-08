00f78912edca3e6509c7c7746046e121911a9d5c
Christopher Albert <albert@alumni.tugraz.at>
2023-08-08 18:54:48 +0200

    Add numpy and scipy dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 # Install dependencies on Debian or Ubuntu systems
-apt-get update && apt-get install -y wget git cmake make python3 \
-    gcc g++ gfortran libnetcdf-dev libnetcdff-dev openmpi-bin libopenmpi-dev
+apt-get update && apt-get install -y wget git cmake make gcc g++ gfortran \
+    libnetcdf-dev libnetcdff-dev openmpi-bin libopenmpi-dev \
+    python3 python3-numpy python3-scipy
