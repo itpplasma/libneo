6138db0a45baf4eb2d386ebe4a98b12a126fcf0f
Christopher Albert <albert@alumni.tugraz.at>
2023-08-08 18:38:12 +0200

    Install MPI dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -2,4 +2,4 @@
 
 # Install dependencies on Debian or Ubuntu systems
 apt-get update && apt-get install -y wget git cmake make python3 \
-    gcc g++ gfortran libnetcdf-dev libnetcdff-dev
+    gcc g++ gfortran libnetcdf-dev libnetcdff-dev openmpi-bin libopenmpi-dev
