76d660af2e0d62f33d797aa5033709da509e0d9e
Christopher Albert <albert@alumni.tugraz.at>
2024-01-19 09:56:29 +0100

    Fix: Float comparison in test

--- tests/source/test_util.f90
+++ tests/source/test_util.f90
@@ -1,6 +1,8 @@
 program test_interpolate
     implicit none
 
+    real(8), parameter :: TOL = 1.0d-12
+
     call test_arange
     call test_linspace
 
@@ -14,7 +16,7 @@ contains
         expected = (/ 0.0d0, 1.0d0, 2.0d0, 3.0d0, 4.0d0 /)
         call arange(0.0d0, 5.0d0, 1.0d0, actual)
 
-        if (any(expected /= actual)) error stop
+        if (maxval(abs(expected - actual)) > TOL) error stop
     end subroutine test_arange
 
     subroutine test_linspace
@@ -25,7 +27,7 @@ contains
         expected = (/ 0.0d0, 1.0d0, 2.0d0, 3.0d0, 4.0d0 /)
         call linspace(0.0d0, 4.0d0, 5, actual)
 
-        if (any(expected /= actual)) error stop
+        if (maxval(abs(expected - actual)) > TOL) error stop
     end subroutine test_linspace
 
 end program test_interpolate
