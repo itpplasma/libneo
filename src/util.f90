8e8849838043846c47fe35c00a13ab9715e16f60
Christopher Albert <albert@alumni.tugraz.at>
2024-01-20 10:30:35 +0000

    Revert linspace due to segfault

--- src/util.f90
+++ src/util.f90
@@ -5,21 +5,18 @@ module util
 
 contains
 
-    function linspace(lo, hi, cnt, excl_lo, excl_hi)
-        real(dp), intent(in) :: lo, hi
-        integer, intent(in) :: cnt
-        integer, intent(in), optional :: excl_lo, excl_hi
-        real(dp) :: linspace(cnt)
-        real(dp) :: step
-        integer :: k, omit_lo, omit_hi
+    subroutine linspace(a, b, n, x)
+        real(8), intent(in) :: a, b
+        integer, intent(in) :: n
+        real(8), dimension(:), intent(out) :: x
 
-        omit_lo = 0
-        if (present(excl_lo)) omit_lo = excl_lo
-        omit_hi = 0
-        if (present(excl_hi)) omit_hi = excl_hi
-        step = (hi - lo) / dble(cnt - 1 + omit_lo + omit_hi)
-        linspace = lo + [(k * step, k = omit_lo, cnt - 1 + omit_lo)]
-        if (omit_hi == 0) linspace(cnt) = hi
-    end function linspace
+        real(8) :: dx
+        integer :: i
+
+        dx = (b - a) / (n - 1)
+        do i = 1, n
+            x(i) = a + (i - 1) * dx
+        end do
+    end subroutine linspace
 
 end module util
