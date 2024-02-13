eb70413a3815a13e59378452b74b2a23ccbb5dbb
Christopher Albert <albert@alumni.tugraz.at>
2024-02-13 09:03:55 +0000

    Add efit_to_boozer module

--- python/setup.py
+++ python/setup.py
@@ -28,6 +28,24 @@ setup(
                            '../src/spline_vmec_data.f90'],
                   include_dirs=[libneo_build_path],
                   libraries=['libneo'],
+                  library_dirs=[libneo_build_path]),
+        Extension('libneo.efit_to_boozer',
+                sources=[
+                     '../../efit_to_boozer/SRC/efit_to_boozer.f90',
+                     '../../efit_to_boozer/SRC/field_divB0.f90',
+                     '../../efit_to_boozer/SRC/efit_to_boozer_mod.f90',
+                     '../../efit_to_boozer/SRC/spline5_RZ.f90',
+                     '../../efit_to_boozer/SRC/spl_three_to_five_mod.f90',
+                     '../../efit_to_boozer/SRC/bdivfree_coul.f90',
+                     '../../efit_to_boozer/SRC/field_line_integration_for_Boozer.f90',
+                     '../../efit_to_boozer/SRC/plag_coeff.f90',
+                     '../../efit_to_boozer/SRC/binsrc.f90',
+                     '../../efit_to_boozer/SRC/rhs.f90',
+                     '../../efit_to_boozer/SRC/spline_and_interpolate_magdata.f90',
+                     'f2py_interfaces/f2py_efit_to_boozer.f90'
+                     ],
+                  include_dirs=[libneo_build_path],
+                  libraries=['libneo'],
                   library_dirs=[libneo_build_path])
-    ],
+    ]
 )
