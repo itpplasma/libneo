144c896114abefad79f58b5d91fde37b5e7de2b9
GeorgGrassler <georg.grassler@tugraz.at>
2024-03-04 12:18:04 +0100

    included build of h5merge in shell script

--- build.sh
+++ build.sh
@@ -2,3 +2,13 @@
 
 cmake --preset default
 cmake --build --preset default
+
+if [ ! -d "tools/h5merge/build" ] ; then
+    echo "Creating 'build' directory..."
+    mkdir tools/h5merge/build
+fi
+
+cd tools/h5merge/build
+cmake ..
+make
+cd -
