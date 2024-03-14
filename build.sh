a822ece3bd1fdd4e2ab6e69e871f9112d4fd50b2
Christopher Albert <albert@alumni.tugraz.at>
2024-03-14 16:01:41 +0100

    pushd and popd in bash

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 cmake --preset default
 cmake --build --preset default
 
 if [ ! -d "tools/h5merge/build" ] ; then
     echo "Creating 'build' directory..."
-    mkdir tools/h5merge/build
+    mkdir -p tools/h5merge/build
 fi
 
-cd tools/h5merge/build
+pushd tools/h5merge/build
 cmake ..
 make
-cd -
+popd
