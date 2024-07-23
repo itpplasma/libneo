dd655095a48cfb19c54c127d298e74fb673dd2a6
Christopher Albert <albert@alumni.tugraz.at>
2024-07-23 09:22:08 +0200

    move into modules

--- src/binsrc.f90
+++ src/binsrc.f90
@@ -1,36 +1,43 @@
-! Finds the index  i  of the array of increasing numbers   p  with dimension  n 
-! which satisfies   p(i-1) <  xi  <  p(i) . Uses binary search algorithm.
-!
-!
-! Note: there is no check if p(nmin) < xi < p(nmax).
-!   If xi <= p(nmin) then nmin+1 is returned.
-!   If xi >= p(nmax) then nmax is returned.
-subroutine binsrc(p, nmin, nmax, xi, i)
-  use libneo_kinds, only : real_kind
-
+module binsrc_sub
   implicit none
 
-  integer, intent(in) :: nmin, nmax
-  integer, intent(out) :: i
-  real(kind=real_kind), intent(in) :: xi
-  real(kind=real_kind), dimension(nmin:nmax), intent(in) :: p
+  contains
 
-  integer :: n, imin, imax, k
+  ! Finds the index  i  of the array of increasing numbers   p  with dimension  n
+  ! which satisfies   p(i-1) <  xi  <  p(i) . Uses binary search algorithm.
+  !
+  !
+  ! Note: there is no check if p(nmin) < xi < p(nmax).
+  !   If xi <= p(nmin) then nmin+1 is returned.
+  !   If xi >= p(nmax) then nmax is returned.
+  subroutine binsrc(p, nmin, nmax, xi, i)
+    use libneo_kinds, only : real_kind
 
-  imin = nmin
-  imax = nmax
-  n = nmax-nmin
+    implicit none
 
-  do k=1,n
-    i = (imax-imin)/2 + imin
-    if (p(i) > xi) then
-      imax = i
-    else
-      imin = i
-    end if
-    if (imax == imin+1) exit
-  end do
+    integer, intent(in) :: nmin, nmax
+    integer, intent(out) :: i
+    real(kind=real_kind), intent(in) :: xi
+    real(kind=real_kind), dimension(nmin:nmax), intent(in) :: p
 
-  i = imax
+    integer :: n, imin, imax, k
 
-end
+    imin = nmin
+    imax = nmax
+    n = nmax-nmin
+
+    do k=1,n
+      i = (imax-imin)/2 + imin
+      if (p(i) > xi) then
+        imax = i
+      else
+        imin = i
+      end if
+      if (imax == imin+1) exit
+    end do
+
+    i = imax
+
+  end
+
+end module binsrc_sub
--- src/plag_coeff.f90
+++ src/plag_coeff.f90
@@ -1,57 +1,64 @@
-!ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
-SUBROUTINE plag_coeff(npoi,nder,x,xp,coef)
-  ! npoi - number of points (determines the order of Lagrange
-  ! polynomial
-  ! which is equal npoi-1)
-  ! nder - number of derivatives computed 0 - function only, 1 - first
-  ! derivative
-  ! x - actual point where function and derivatives are evaluated
-  ! xp(npoi) - array of points where function is known
-  ! coef(0:nder,npoi) - weights for computation of function and
-  ! derivatives,
-  ! f=sum(fun(1:npoi)*coef(0,1:npoi) gives the function value
-  ! df=sum(fun(1:npoi)*coef(1,1:npoi) gives the derivative value
+module plag_coeff_sub
+  implicit none
 
-  use libneo_kinds, only : real_kind
+  contains
 
-  INTEGER, INTENT(in) :: npoi,nder
-  real(kind=real_kind), INTENT(in) :: x
-  real(kind=real_kind), DIMENSION(npoi), INTENT(in) :: xp
-  real(kind=real_kind), DIMENSION(0:nder,npoi), INTENT(out) :: coef
-  real(kind=real_kind), DIMENSION(:), ALLOCATABLE :: dummy
+  !ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+  SUBROUTINE plag_coeff(npoi,nder,x,xp,coef)
+    ! npoi - number of points (determines the order of Lagrange
+    ! polynomial
+    ! which is equal npoi-1)
+    ! nder - number of derivatives computed 0 - function only, 1 - first
+    ! derivative
+    ! x - actual point where function and derivatives are evaluated
+    ! xp(npoi) - array of points where function is known
+    ! coef(0:nder,npoi) - weights for computation of function and
+    ! derivatives,
+    ! f=sum(fun(1:npoi)*coef(0,1:npoi) gives the function value
+    ! df=sum(fun(1:npoi)*coef(1,1:npoi) gives the derivative value
 
-  INTEGER :: i,k,j
-  real(kind=real_kind) :: fac
+    use libneo_kinds, only : real_kind
 
-  DO i=1,npoi
-    coef(0,i) = 1.d0
-    DO k=1,npoi
-      IF(k.EQ.i) CYCLE
-      coef(0,i) = coef(0,i)*(x-xp(k))/(xp(i)-xp(k))
-    END DO
-  END DO
+    INTEGER, INTENT(in) :: npoi,nder
+    real(kind=real_kind), INTENT(in) :: x
+    real(kind=real_kind), DIMENSION(npoi), INTENT(in) :: xp
+    real(kind=real_kind), DIMENSION(0:nder,npoi), INTENT(out) :: coef
+    real(kind=real_kind), DIMENSION(:), ALLOCATABLE :: dummy
 
-  IF (nder.EQ.0) RETURN
+    INTEGER :: i,k,j
+    real(kind=real_kind) :: fac
 
-  ALLOCATE(dummy(npoi))
-
-  DO i=1,npoi
-    dummy = 1.d0
-    dummy(i) = 0.d0
-    DO k=1,npoi
-      IF(k.EQ.i) CYCLE
-      fac = (x-xp(k))/(xp(i)-xp(k))
-      DO j=1,npoi
-        IF (j.EQ.k) THEN
-          dummy(j) = dummy(j)/(xp(i)-xp(k))
-        ELSE
-          dummy(j) = dummy(j)*fac
-        END IF
+    DO i=1,npoi
+      coef(0,i) = 1.d0
+      DO k=1,npoi
+        IF(k.EQ.i) CYCLE
+        coef(0,i) = coef(0,i)*(x-xp(k))/(xp(i)-xp(k))
       END DO
     END DO
-    coef(1,i) = SUM(dummy)
-  END DO
 
-  DEALLOCATE(dummy)
+    IF (nder.EQ.0) RETURN
 
-END SUBROUTINE plag_coeff
+    ALLOCATE(dummy(npoi))
+
+    DO i=1,npoi
+      dummy = 1.d0
+      dummy(i) = 0.d0
+      DO k=1,npoi
+        IF(k.EQ.i) CYCLE
+        fac = (x-xp(k))/(xp(i)-xp(k))
+        DO j=1,npoi
+          IF (j.EQ.k) THEN
+            dummy(j) = dummy(j)/(xp(i)-xp(k))
+          ELSE
+            dummy(j) = dummy(j)*fac
+          END IF
+        END DO
+      END DO
+      coef(1,i) = SUM(dummy)
+    END DO
+
+    DEALLOCATE(dummy)
+
+  END SUBROUTINE plag_coeff
+
+end module plag_coeff_sub
