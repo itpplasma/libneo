f09ac9a1fa9a473aca1f68950e3630186c15ff02
GeorgGrassler <georg.grassler@tugraz.at>
2024-08-13 17:30:40 +0200

    seperated f and df of polylag

--- src/polylag/polylag_5.f90
+++ src/polylag/polylag_5.f90
@@ -38,19 +38,8 @@ enddo
 end subroutine find_node_index
 
 
+!> Lagrange interpolation on uniform (increasingly ordered) mesh 1D
 subroutine plag1d(x, xp, fp, dx, f, dfdx)
-! 1D interpolation by means of Lagrange polynomial
-! the power 5 is fixed strictly:
-! uniform mesh (increasingly ordered) in all dimensions is implied
-!
-! Input parameters:
-!   x - coordinate of the point for interpolation
-!   dx - h_x
-!   xp - vertices of stencil
-!
-! Output parameters:
-! polyl1d - polynomial itself
-! poly1x - its derivative
 real(dp), intent(in) :: x, dx
 real(dp), dimension(mp), intent(in) :: xp, fp
 real(dp), intent(out) :: f, dfdx
@@ -69,53 +58,67 @@ enddo
 end subroutine plag1d
 
 
+!> Lagrange interpolation on uniform (increasingly ordered) mesh 3D
 subroutine plag3d(x, y, z, xp, yp, zp, fp, dx, dy, dz, &
                   f, dfdx, dfdy, dfdz)
-! 3D interpolation by means of Lagrange polynomial
-! the power 5 is fixed strictly:
-! uniform mesh (increasingly ordered) in all dimensions is implied
-!
-! Input parameters:
-!   x,y,z - coordinates of the point for interpolation
-!   dx,dy,dz - steps in each direction
-!   xp,yp,zp - vertices of stencil
-!
-! Output parameters:
-! polyl3d - polynomial itself
-! poly1x - its x-derivative
-! poly1y - its y-derivative
-! poly1z - its z-derivative
-real(dp), intent(in) :: x, y, z, dx, dy, dz
-real(dp), dimension(mp,mp,mp), intent(in) :: fp
-real(dp), dimension(mp), intent(in) :: xp, yp, zp
-real(dp), intent(out) :: f, dfdx, dfdy, dfdz
+    real(dp), intent(in) :: x, y, z, dx, dy, dz
+    real(dp), dimension(mp), intent(in) :: xp, yp, zp
+    real(dp), dimension(mp,mp,mp), intent(in) :: fp
+    real(dp), intent(out) :: f, dfdx, dfdy, dfdz
 
-real(dp), dimension(mp) :: x_coefs, y_coefs, z_coefs
-real(dp), dimension(mp) :: dx_coefs, dy_coefs, dz_coefs
-integer :: i, j, k
+    call f_plag3d(x, y, z, xp, yp, zp, fp, dx, dy, dz, f)
+    call df_plag3d(x, y, z, xp, yp, zp, fp, dx, dy, dz, dfdx, dfdy, dfdz)
+end subroutine plag3d
 
-x_coefs = calc_coefs(x,xp,dx)
-y_coefs = calc_coefs(y,yp,dy)
-z_coefs = calc_coefs(z,zp,dz)
-dx_coefs = calc_derivative_coefs(x,xp,dx)
-dy_coefs = calc_derivative_coefs(y,yp,dy)
-dz_coefs = calc_derivative_coefs(z,zp,dz)
+subroutine f_plag3d(x, y, z, xp, yp, zp, fp, dx, dy, dz, f)
+    real(dp), intent(in) :: x, y, z, dx, dy, dz
+    real(dp), dimension(mp), intent(in) :: xp, yp, zp
+    real(dp), dimension(mp,mp,mp), intent(in) :: fp
+    real(dp), intent(out) :: f
 
-f = 0.0_dp
-dfdx = 0.0_dp
-dfdy = 0.0_dp
-dfdz = 0.0_dp
-do k=1,mp
-    do j=1,mp
-        do i=1,mp
-            f = f + fp(i,j,k) * x_coefs(i) * y_coefs(j) * z_coefs(k)
-            dfdx = dfdx + fp(i,j,k) * dx_coefs(i) * y_coefs(j) * z_coefs(k)
-            dfdy = dfdy + fp(i,j,k) * x_coefs(i) * dy_coefs(j) * z_coefs(k)
-            dfdz = dfdz + fp(i,j,k) * x_coefs(i) * y_coefs(j) * dz_coefs(k)
+    real(dp), dimension(mp) :: x_coefs, y_coefs, z_coefs
+    integer :: i, j, k
+    x_coefs = calc_coefs(x,xp,dx)
+    y_coefs = calc_coefs(y,yp,dy)
+    z_coefs = calc_coefs(z,zp,dz)
+    f = 0.0_dp
+    do k=1,mp
+        do j=1,mp
+            do i=1,mp
+                f = f + fp(i,j,k) * x_coefs(i) * y_coefs(j) * z_coefs(k)
+            enddo
         enddo
     enddo
-enddo
-end subroutine plag3d
+end subroutine f_plag3d
+
+subroutine df_plag3d(x, y, z, xp, yp, zp, fp, dx, dy, dz, dfdx, dfdy, dfdz)
+    real(dp), intent(in) :: x, y, z, dx, dy, dz
+    real(dp), dimension(mp), intent(in) :: xp, yp, zp
+    real(dp), dimension(mp,mp,mp), intent(in) :: fp
+    real(dp), intent(out) :: dfdx, dfdy, dfdz
+
+    real(dp), dimension(mp) :: x_coefs, y_coefs, z_coefs
+    real(dp), dimension(mp) :: dx_coefs, dy_coefs, dz_coefs
+    integer :: i, j, k
+    x_coefs = calc_coefs(x,xp,dx)
+    y_coefs = calc_coefs(y,yp,dy)
+    z_coefs = calc_coefs(z,zp,dz)
+    dx_coefs = calc_derivative_coefs(x,xp,dx)
+    dy_coefs = calc_derivative_coefs(y,yp,dy)
+    dz_coefs = calc_derivative_coefs(z,zp,dz)
+    dfdx = 0.0_dp
+    dfdy = 0.0_dp
+    dfdz = 0.0_dp
+    do k=1,mp
+        do j=1,mp
+            do i=1,mp
+                dfdx = dfdx + fp(i,j,k) * dx_coefs(i) * y_coefs(j) * z_coefs(k)
+                dfdy = dfdy + fp(i,j,k) * x_coefs(i) * dy_coefs(j) * z_coefs(k)
+                dfdz = dfdz + fp(i,j,k) * x_coefs(i) * y_coefs(j) * dz_coefs(k)
+            enddo
+        enddo
+    enddo
+end subroutine df_plag3d
 
 
 function calc_coefs(u,up,du) result(coefs)
