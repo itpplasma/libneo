f66d6da57cdfa7a6422a703fe63730504ce67471
GeorgGrassler <georg.grassler@tugraz.at>
2024-09-18 12:52:35 +0200

    link hdf5 into hdf5tools directly

--- test/field/setup_test_jorek_field.f90
+++ test/field/setup_test_jorek_field.f90
@@ -1,7 +1,7 @@
 program setup_test_jorek_field
 use, intrinsic :: iso_fortran_env, only: dp => real64
 use util_for_test, only: print_test, print_ok, print_fail
-!use hdf5_tools, only: h5_init, h5_deinit
+use hdf5_tools, only: h5_init, h5_deinit
 
 implicit none
 
@@ -15,8 +15,8 @@ contains
 subroutine create_straight_field_jorek_file()
     call print_test("setup_test_jorek_field")
     
-    !call h5_init()
-    !call h5_deinit()
+    call h5_init()
+    call h5_deinit()
     
     call print_ok
 end subroutine create_straight_field_jorek_file
