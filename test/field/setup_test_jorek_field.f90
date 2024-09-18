8379b261ea0e93b531f92665805a3c571f8b2349
GeorgGrassler <georg.grassler@tugraz.at>
2024-09-18 15:20:11 +0200

    homogenous mockup field jorek output

--- test/field/setup_test_jorek_field.f90
+++ test/field/setup_test_jorek_field.f90
@@ -6,42 +6,44 @@ use hdf5_tools, only: hid_t, h5_init, h5_create, h5_add, h5_close, h5_deinit
 implicit none
 
 
-call create_homogenous_field_jorek_output()
+call create_mockup_jorek_output()
 
 
 contains
 
 
-subroutine create_homogenous_field_jorek_output()
+subroutine create_mockup_jorek_output()
     integer, parameter :: n_var = 17, n_R = 100, n_Z = 100, n_phi = 33
     integer(hid_t) :: file_id
     character(len=*), parameter :: filename = 'test_jorek_output.h5'
     character(len=100) :: comment, description
-    integer :: ndim(3)
-    integer, parameter :: index_now = 40000
+    integer :: dimensions(3)
+    integer, parameter :: ndim = 3, index_now = 1000
     real(dp), dimension(:,:,:,:), allocatable :: values
-    real(dp), parameter :: t_now = 198906, time = 0.0916456
+    real(dp), parameter :: t_now = 10000, time = 0.01
     character(len=5), parameter :: variables(17) = (/"p","h","i"," "," "," "," "," ", & 
                                                    " "," "," "," ","A","_","R"," "," "/)
 
     comment = 'Output produced by jorek2_postproc command "rectangular_torus"'
     description = 'Diagnostic export from JOREK'
-    ndim = (/n_R, n_Z, n_phi/)
-    allocate(values(n_var, n_R, n_Z, n_phi))
+    dimensions = (/n_phi, n_Z, n_R/)
+    allocate(values(n_phi, n_Z, n_R, n_var))
+    call get_homogenous_field(values)
     
-    call print_test("create_homogenous_field_jorek_output")
+    call print_test("create_mockup_jorek_output")
     call h5_init()
     call h5_create(filename, file_id)
     call h5_add(file_id, 'comment', comment)
     call h5_add(file_id, 'description', description)
+    call h5_add(file_id, 'dim', dimensions, lbounds=(/1/), ubounds=(/3/), &
+                                            comment='quantity nR nZ nPhi', unit='')
     call h5_add(file_id, 'index_now', index_now)
     call h5_add(file_id, 'n_var', n_var)
-    call h5_add(file_id, 'dim', ndim, lbounds=(/1/), ubounds=(/4/), &
-                                      comment='quantity nR nZ nPhi', unit='')
+    call h5_add(file_id, 'ndim', ndim)
     call h5_add(file_id, 't_now', t_now)
     call h5_add(file_id, 'time', time)
     call h5_add(file_id, 'values', values, lbounds=(/1,1,1,1/), &
-                                           ubounds=(/n_var,n_R,n_Z,n_phi/), &
+                                           ubounds=(/n_phi, n_Z, n_R, n_var/), &
                                            comment='Magnetic field values', unit='T')
     call h5_add(file_id, 'variables', variables, lbounds=(/1/), ubounds=(/17/))
     call h5_close(file_id)
@@ -49,6 +51,14 @@ subroutine create_homogenous_field_jorek_output()
     call h5_deinit()
     
     call print_ok
-end subroutine create_homogenous_field_jorek_output
+end subroutine create_mockup_jorek_output
+
+subroutine get_homogenous_field(values)
+    real(dp), dimension(:,:,:,:), intent(out) :: values
+
+    values = 0.0_dp
+    values(:, :, :, 13) = 1.0_dp
+end subroutine get_homogenous_field
+
 
 end program setup_test_jorek_field
