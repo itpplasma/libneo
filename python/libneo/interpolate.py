e52cd7cb4c7895f1248c7bf9691191ad67140b1e
Christopher Albert <albert@alumni.tugraz.at>
2024-11-03 10:57:12 +0000

    interpolate

---
+++ python/libneo/interpolate.py
@@ -0,0 +1,1 @@
+../../build/interpolate.py
---
+++ test/python/test_interpolate.py
@@ -0,0 +1,14 @@
+from libneo.interpolate import Interpolate
+
+def test_construct_splines_1d():
+    import libneo
+    x_min = 0.0
+    x_max = 1.0
+    y = np.linspace(0, 1, 10)
+    order = 5
+    periodic = False
+    spl = Interpolate.construct_splines_1d(x_min, x_max, y, order, periodic)
+    assert type(spl) == Interpolate.SplineData1D
+
+if __name__ == '__main__':
+    test_construct_splines_1d()
