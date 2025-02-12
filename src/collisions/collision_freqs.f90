eb57e30aa04c9807884083c0dc3fd8f47e822061
Markus Markl <markl@tugraz.at>
2025-02-12 09:59:58 +0100

    Implement Coulomb logarithm

--- src/collisions/collision_freqs.f90
+++ src/collisions/collision_freqs.f90
@@ -6,9 +6,45 @@ module libneo_collisions
 
     contains
 
-    subroutine calc_coulomb_log()
+    subroutine calc_coulomb_log(interaction_type, temp_a, dens_a, mass_a, charge_num_a, &
+        temp_b, dens_b,  mass_b, charge_num_b, coulomb_log)
+        ! determines the Coulomb logarithm for a given interaction type
+        ! units are in cgs (temperature is in eV)
+        ! interaction_type: "ee", "ei", "ie", "ii"
+        ! species a is electrons for ee, ei and ie; species b is ions
+
+        use math_constants, only : m_e, m_p
 
         implicit none
+
+        character(len=2), intent(in) :: interaction_type
+        real(kind=real_kind), intent(in) :: temp_a, dens_a, temp_b, dens_b
+        real(kind=real_kind), intent(in) :: mass_a, mass_b
+        integer, intent(in) :: charge_num_a, charge_num_b
+        real(kind=real_kind), intent(out) :: coulomb_log
+
+        select case (trim(interaction_type))
+            case ("ee")
+                coulomb_log = 23.5 - log(sqrt(dens_a) * temp_a**(-5.0/4.0)) - &
+                    sqrt(10**(-5.0) + (log(temp_a) - 2.0d0)**2.0/16.0)
+            case ("ei", "ie")
+                if (temp_b * m_e / mass_b < temp_a .and. temp_a < 10.0 * charge_num_b**2.0) then
+                    coulomb_log = 23.0 - log(sqrt(dens_a) * charge_num_b * temp_a**(-1.5))
+                elseif (temp_b * m_e / mass_b < 10.0 * charge_num_b**2.0 .and. 10.0 * charge_num_b**2.0 < temp_a) then
+                    coulomb_log = 24.0 - log(sqrt(dens_a) * temp_a**(-1.0))
+                elseif (temp_a < temp_b * m_e / mass_b) then
+                    coulomb_log = 16.0 - log(sqrt(dens_b) * temp_b**(-1.5) * charge_num_b**2.0 * mass_b/m_p)
+                end if
+            case ("ii")
+                coulomb_log = 23.0 - log(charge_num_a * charge_num_b * (mass_a + mass_b) / (mass_a * temp_b + mass_b * temp_a) &
+                    * sqrt(dens_a * charge_num_a**2.0 / temp_a + dens_b * charge_num_b**2.0 / temp_b))
+            case default
+                print *, "Unknown interaction type"
+        end select
+
+        if (coulomb_log < 0.0 .or. coulomb_log > 30.0) then
+            stop "Coulomb logarithm has unexpected value"
+        end if
 
     end subroutine
 
@@ -35,7 +71,7 @@ module libneo_collisions
         nue_0 = 4.0d0 * pi *charge_a**2 * charge_b**2 * coulomb_log * dens_b / &
             (mass_a**2 * vel**3)
         z_ab = mass_b * vel**2 /(2.0d0 * temp_b * ev_to_cgs)
-        
+
         psi_of_x = lig(p, z_ab)
 
         coll_freq = nue_0
--- test/collisions/test_collision_freqs.f90
+++ test/collisions/test_collision_freqs.f90
@@ -1,12 +1,12 @@
 program test_perp_coll_frequency
 
     use libneo_kinds, only : real_kind
-    use libneo_collisions, only: calc_perp_coll_freq_slow_limit_ee, calc_perp_coll_freq_fast_limit_ee
+    use libneo_collisions, only: calc_perp_coll_freq_slow_limit_ee, calc_perp_coll_freq_fast_limit_ee, &
+        calc_coulomb_log
 
     implicit none
 
-    print *, "works!"
-    !call test_calc_coulomb_log
+    call test_calc_coulomb_log
     !call test_calc_perp_coll_freq
     !call test_calc_perp_coll_freq_slow_limit
     !call test_calc_perp_coll_freq_fast_limit
@@ -14,6 +14,55 @@ program test_perp_coll_frequency
     !call test_calc_perp_coll_freq_slow_limit
 
     contains
+
+    subroutine test_calc_coulomb_log
+
+        use math_constants, only: m_e, m_p, m_D
+        implicit none
+
+        character(len=2) :: interaction_type
+        real(kind=real_kind) :: temp_a, dens_a, temp_b, dens_b
+        real(kind=real_kind) :: mass_a, mass_b
+        integer :: charge_num_a, charge_num_b
+        real(kind=real_kind) :: coulomb_log
+
+        ! electron-electron interaction
+        temp_a = 1.0d3
+        dens_a = 1.0d13
+        mass_a = m_e
+        charge_num_a = 0
+        temp_b = 0d0
+        dens_b = 0d0
+        mass_b = 0d0
+        charge_num_b = 0
+
+        interaction_type = "ee"
+        call calc_coulomb_log(interaction_type, temp_a, dens_a, mass_a, charge_num_a, &
+        temp_b, dens_b,  mass_b, charge_num_b, coulomb_log)
+        print *, "interaction_type ", interaction_type, ": ", "coulomb_log = ", coulomb_log
+
+        ! electron-ion interaction
+        temp_b = 1d3
+        dens_b = dens_a
+        mass_b = m_D
+        charge_num_b = 1
+
+        interaction_type = "ei"
+        call calc_coulomb_log(interaction_type, temp_a, dens_a, mass_a, charge_num_a, &
+        temp_b, dens_b,  mass_b, charge_num_b, coulomb_log)
+        print *, "interaction_type ", interaction_type, ": ", "coulomb_log = ", coulomb_log
+
+        ! ion-ion interaction
+        mass_a = m_p
+        charge_num_a = 1
+
+        interaction_type = "ii"
+        call calc_coulomb_log(interaction_type, temp_a, dens_a, mass_a, charge_num_a, &
+        temp_b, dens_b,  mass_b, charge_num_b, coulomb_log)
+        print *, "interaction_type ", interaction_type, ": ", "coulomb_log = ", coulomb_log
+        
+
+    end subroutine
 
     subroutine test_calc_perp_coll_freq_fast_limit
 
