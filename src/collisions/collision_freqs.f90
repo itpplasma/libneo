6d6402df6873caa167014a38ea4a07a1762de9d9
Markus Markl <markl@tugraz.at>
2025-02-12 09:00:23 +0100

    Implement fortran stdlib and lower incomplete gamma function

--- src/collisions/collision_freqs.f90
+++ src/collisions/collision_freqs.f90
@@ -1,10 +1,8 @@
 module libneo_collisions
 
-    use, intrinsic :: iso_fortran_env, only: dp => real64
-    use libneo_kinds, only : real_kind, complex_kind
+    use libneo_kinds, only : real_kind
     use math_constants, only : pi, ev_to_cgs
     implicit none
-
 
     contains
 
@@ -21,6 +19,8 @@ module libneo_collisions
         ! a is the test species, b is the background species
         ! units are in cgs (temperature is in eV)
 
+        use stdlib_specialfunctions_gamma, only: lig => lower_incomplete_gamma
+
         implicit none
         real(kind=real_kind), intent(in) :: vel, mass_a, mass_b, charge_a, charge_b, &
             dens_b, temp_b, coulomb_log
@@ -29,10 +29,14 @@ module libneo_collisions
         real(kind=real_kind) :: nue_0, z_ab
         real(kind=real_kind) :: psi_of_x
 
+        real(kind=real_kind) :: p
+        p = 1.5d0
+
         nue_0 = 4.0d0 * pi *charge_a**2 * charge_b**2 * coulomb_log * dens_b / &
             (mass_a**2 * vel**3)
         z_ab = mass_b * vel**2 /(2.0d0 * temp_b * ev_to_cgs)
-        psi_of_x = 0.0d0
+        
+        psi_of_x = lig(p, z_ab)
 
         coll_freq = nue_0
 
@@ -47,7 +51,7 @@ module libneo_collisions
         real(kind=real_kind), intent(in) :: vel, dens_e, temp_b, coulomb_log
         real(kind=real_kind), intent(out) :: coll_freq
 
-        coll_freq = 5.8 * 10**-6 * dens_e * coulomb_log * temp_b**-0.5 * (m_e*vel**2 * 0.5)**-1.0
+        coll_freq = 5.8 * 10**(-6d0) * dens_e * coulomb_log * temp_b**-0.5 * (m_e*vel**2 * 0.5 /ev_to_cgs)**(-1.0d0)
 
     end subroutine
 
@@ -59,7 +63,7 @@ module libneo_collisions
         real(kind=real_kind), intent(in) :: vel, dens_e, coulomb_log
         real(kind=real_kind), intent(out) :: coll_freq
 
-        coll_freq = 7.7 * 10**-6 * dens_e * coulomb_log * (m_e*vel**2 * 0.5)**-1.5
+        coll_freq = 7.7 * 10**(-6d0) * dens_e * coulomb_log * (m_e * vel**2.0d0 * 0.5 / ev_to_cgs)**(-1.5d0)
 
     end subroutine
 
--- test/collisions/test_collision_freqs.f90
+++ test/collisions/test_collision_freqs.f90
@@ -1,9 +1,7 @@
 program test_perp_coll_frequency
 
-    use, intrinsic :: iso_fortran_env, only: dp => real64
-    use util_for_test
-    use libneo_collisions, only: calc_perp_coll_freq, calc_perp_coll_freq_slow_limit, &
-    calc_perp_coll_freq_fast_limit, calc_coulomb_log
+    use libneo_kinds, only : real_kind
+    use libneo_collisions, only: calc_perp_coll_freq_slow_limit_ee, calc_perp_coll_freq_fast_limit_ee
 
     implicit none
 
@@ -12,5 +10,24 @@ program test_perp_coll_frequency
     !call test_calc_perp_coll_freq
     !call test_calc_perp_coll_freq_slow_limit
     !call test_calc_perp_coll_freq_fast_limit
+    call test_calc_perp_coll_freq_fast_limit
+    !call test_calc_perp_coll_freq_slow_limit
+
+    contains
+
+    subroutine test_calc_perp_coll_freq_fast_limit
+
+        implicit none
+        real(kind=real_kind) :: vel, dens_e, coulomb_log
+        real(kind=real_kind) :: coll_freq = 0.0d0
+
+        vel = 1.0e8
+        dens_e = 1.0e13
+        coulomb_log = 15.0
+
+        call calc_perp_coll_freq_fast_limit_ee(vel, dens_e, coulomb_log, coll_freq)
+        print *, "coll_freq = ", coll_freq
+
+    end subroutine
 
 end program
