45caa9214080de95a28a2eb808d581bd320c5a72
Markus Markl <markl@tugraz.at>
2025-02-17 07:50:51 +0100

    Add species module

---
+++ src/species/species.f90
@@ -0,0 +1,58 @@
+module libneo_species
+
+    use libneo_kinds, only: real_kind
+
+    implicit none
+
+    type species
+        character(len=16) :: name
+        real(kind=real_kind) :: temp
+        real(kind=real_kind) :: dens
+        real(kind=real_kind) :: mass
+        integer :: charge_num
+    end type species
+
+    contains
+
+    subroutine init_electron_species(temp, dens, electron)
+
+        use math_constants, only: m_e
+        implicit none
+        real(kind=real_kind), intent(in) :: temp, dens
+        type(species), intent(out) :: electron
+
+        electron%name = "electron"
+        electron%temp = temp
+        electron%dens = dens
+        electron%mass = m_e
+        electron%charge_num = -1
+
+    end subroutine
+
+    subroutine init_deuterium_species(temp, dens, deuterium)
+
+        use math_constants, only: m_D
+        implicit none
+        real(kind=real_kind), intent(in) :: temp, dens
+        type(species), intent(out) :: deuterium
+
+        deuterium%name = "deuterium"
+        deuterium%temp = temp
+        deuterium%dens = dens
+        deuterium%mass = m_D
+        deuterium%charge_num = 1
+
+    end subroutine
+
+    subroutine init_deuterium_plasma(temp_e, temp_D, dens, deuterium_plasma)
+
+        implicit none
+        real(kind=real_kind), intent(in) :: temp_e, temp_D, dens
+        type(species), intent(out) :: deuterium_plasma(2)
+
+        call init_electron_species(temp_e, dens, deuterium_plasma(1))
+        call init_deuterium_species(temp_D, dens, deuterium_plasma(2))
+
+    end subroutine
+
+end module
--- src/transport/transport.f90
+++ src/transport/transport.f90
@@ -5,34 +5,63 @@ module libneo_transport
     implicit none
 
     logical :: debugging = .false.
+    real (kind = real_kind), allocatable, dimension ( : ) :: w
+    real (kind = real_kind), allocatable, dimension ( : ) :: x
+    logical :: write_to_file = .false.
+    integer :: gauss_laguerre_order
+
+    type species
+        character(len = 16) :: name
+        real(kind = real_kind) :: temp
+        real(kind = real_kind) :: dens
+        real(kind = real_kind) :: mass
+        integer :: charge_num
+    end type species
 
     contains
 
-    subroutine D_one_over_nu_11()
+    subroutine init_gauss_laguerre_integration(alpha)
 
-        use libneo_collisions, only: calc_coulomb_log, calc_perp_coll_freq
+        ! calculate gauss laguerre weights and abscissas
+        ! alpha is the exponent of the monomial in the integration
+        ! alpha is 5/2 for D11 and 7/2 for D12
+
+        implicit none
+        ! calculate Gauss-Laguerre weights and abscissas
+        real(kind = real_kind) :: alpha
+        real (kind = real_kind) :: a, b, beta
+        
+        
+        beta = 0.0D+00 !dummy
+        a = 0.0d0
+        b = 1.0d0
+
+        allocate(w(gauss_laguerre_order), x(gauss_laguerre_order))
+
+        call calculate_gauss_laguerre_rule(gauss_laguerre_order, alpha, beta, &
+            a, b, x, w, write_to_file)
+
+    end subroutine
+
+    subroutine D_one_over_nu_11e(D11)
+
+        use libneo_collisions, only: calc_perp_coll_freq
 
         implicit none
 
-        ! calculate Gauss-Laguerre weights and abscissas
-        real (kind = real_kind) :: a, alpha, b, beta
-        integer :: order
-        real ( kind = real_kind ), allocatable, dimension ( : ) :: w
-        real ( kind = real_kind ), allocatable, dimension ( : ) :: x
-        logical :: write_to_file
+        real(kind = real_kind) :: coll_freq, coll_freq_tot
+        real(kind = real_kind), intent(out) :: D11
         
-        beta = 0.0D+00 !dummy
-        order = 100
-        alpha = 5.0d0/2.0d0
-        a = 0.0d0
-        b = 1.0d0
-        write_to_file = .false.
+        integer :: i
+        
+        D11 = 0.0d0
 
-        allocate(w(order), x(order))
+        do i = 1, gauss_laguerre_order
+            vel = sqrt(2 * x(i) * temp_a / mass_a)
+            
+            D11 = D11 + w(i) * x(i)**2.0d0
+        end do
 
-        call calculate_gauss_laguerre_rule(order, alpha, beta, a, b, x, w, write_to_file)
-
-        deallocate(w, x)
 
     end subroutine
 
