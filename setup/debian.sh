dc95eaf338ad66f6b9dcb1a0a831c751cb6edbf1
Christopher Albert <albert@tugraz.at>
2025-05-05 12:01:03 +0200

    hotfix: fix build problems on Ubuntu 24.04

--- setup/debian.sh
+++ setup/debian.sh
@@ -3,4 +3,5 @@
 # Install dependencies on Debian or Ubuntu systems
 apt-get update && apt-get install -y wget git cmake make gcc g++ gfortran \
     libnetcdf-dev libnetcdff-dev openmpi-bin libopenmpi-dev libfftw3-dev \
-    python3 python3-pip python3-numpy python3-scipy ninja-build
+    python3 python3-pip python3-numpy python3-scipy ninja-build libgsl-dev \
+    libopenblas-dev
