54584f6e304761d332aad2ae86bd1c3758baf7ad
Christopher Albert <albert@tugraz.at>
2025-08-26 15:43:57 +0200

    refactor: standardize naming conventions for Nemov and Biot-Savart routines

--- src/magfie/coil_convert.f90
+++ src/magfie/coil_convert.f90
@@ -2,7 +2,7 @@ program coil_convert
   use iso_fortran_env, only: error_unit
   use coil_tools, only: coil_t, coil_init, coil_deinit, coils_append, &
     process_fixed_number_of_args, check_number_of_args, &
-    coils_write_AUG, coils_read_AUG, coils_write_Nemov, coils_read_Nemov, &
+    coils_write_AUG, coils_read_AUG, coils_writenemov, coils_readnemov, &
     coils_write_GPEC, coils_read_GPEC
 
   implicit none
@@ -51,9 +51,9 @@ program coil_convert
       call coils_append(coils, more_coils)
     end do
   else if (in_type == 'Nemov') then
-    call coils_read_Nemov(trim(in_files(1)), coils)
+    call coils_readnemov(trim(in_files(1)), coils)
     do kc = 2, in_ncoil
-      call coils_read_Nemov(trim(in_files(kc)), more_coils)
+      call coils_readnemov(trim(in_files(kc)), more_coils)
       call coils_append(coils, more_coils)
     end do
   else
@@ -94,10 +94,10 @@ program coil_convert
         error stop
       end if
       do kc = 1, out_ncoil
-        call coils_write_Nemov(trim(out_files(kc)), coils(kc:kc))
+        call coils_writenemov(trim(out_files(kc)), coils(kc:kc))
       end do
     else
-      call coils_write_Nemov(trim(out_files(1)), coils)
+      call coils_writenemov(trim(out_files(1)), coils)
     end if
   else
     write (error_unit, '("Unknown output type ", a, ".")') trim(out_type)
