4d26f478413b2e4660d450f51c0469df9dc9d0b9
Christopher Albert <albert@alumni.tugraz.at>
2025-09-30 23:15:47 +0200

    Reintroduce deterministic geoflux field checks

--- test/source/test_geoflux.f90
+++ test/source/test_geoflux.f90
@@ -3,6 +3,8 @@ program test_geoflux
     use, intrinsic :: ieee_arithmetic, only : ieee_is_finite
     use geoflux_coordinates, only : geoflux_to_cyl, cyl_to_geoflux
     use geoflux_field, only : spline_geoflux_data, splint_geoflux_field
+    use field_sub, only : field_eq, psif
+    use field_eq_mod, only : psi_axis
 
     implicit none
 
@@ -13,7 +15,15 @@ program test_geoflux
     real(dp) :: x_cyl(3)
     real(dp) :: max_diff
     real(dp) :: Acov(3), hcov(3), Bmod
-    real(dp) :: unit_length
+    real(dp) :: jac(3,3)
+    real(dp) :: Br, Bphi, Bz
+    real(dp) :: dBrdR, dBrdp, dBrdZ
+    real(dp) :: dBpdR, dBpdp, dBpdZ
+    real(dp) :: dBzdR, dBzdp, dBzdZ
+    real(dp) :: Bmod_expected
+    real(dp) :: hcov_expected(3)
+    real(dp) :: psi_expected
+    real(dp) :: tol_field
 
     call spline_geoflux_data(geqdsk_file, 64, 128)
 
@@ -36,7 +46,7 @@ program test_geoflux
     end if
 
     x_geo = [0.8_dp, 0.9_dp, 2.3_dp]
-    call geoflux_to_cyl(x_geo, x_cyl)
+    call geoflux_to_cyl(x_geo, x_cyl, jac)
     if (x_cyl(1) <= 0.0_dp) then
         write(*,*) 'Non-positive R coordinate on outer surface.'
         error stop
@@ -46,6 +56,24 @@ program test_geoflux
         error stop
     end if
 
+    call field_eq(x_cyl(1), x_cyl(2), x_cyl(3), Br, Bphi, Bz, &
+                  dBrdR, dBrdp, dBrdZ, dBpdR, dBpdp, dBpdZ, &
+                  dBzdR, dBzdp, dBzdZ)
+
+    Bmod_expected = sqrt(Br*Br + Bphi*Bphi + Bz*Bz)
+    if (Bmod_expected <= 0.0_dp) then
+        write(*,*) 'Reference Bmod not positive: ', Bmod_expected
+        error stop
+    end if
+
+    hcov_expected(1) = (Br*jac(1,1) + Bz*jac(3,1)) / Bmod_expected
+    hcov_expected(2) = (Br*jac(1,2) + Bz*jac(3,2)) / Bmod_expected
+    hcov_expected(3) = (Bphi * x_cyl(1)) / Bmod_expected
+
+    psi_expected = psif - psi_axis
+
+    tol_field = 5.0d-10
+
     call splint_geoflux_field(x_geo(1), x_geo(2), x_geo(3), Acov, hcov, Bmod)
 
     if (.not. ieee_is_finite(Bmod) .or. Bmod <= 0.0_dp) then
@@ -53,14 +81,26 @@ program test_geoflux
         error stop
     end if
 
-    unit_length = maxval(abs(hcov))
-    if (.not. (unit_length < 1.0d6 .and. unit_length > 0.0d0)) then
-        write(*,*) 'Unexpected hcov magnitude: ', unit_length
+    if (abs(Bmod - Bmod_expected) > tol_field) then
+        write(*,*) 'Bmod mismatch. expected=', Bmod_expected, ' got=', Bmod
+        error stop
+    end if
+
+    if (maxval(abs(hcov - hcov_expected)) > tol_field) then
+        write(*,*) 'Covariant unit vector mismatch.'
+        write(*,*) 'expected=', hcov_expected
+        write(*,*) 'got=', hcov
         error stop
     end if
 
     if (.not. ieee_is_finite(Acov(3))) then
         write(*,*) 'A_phi is not finite: ', Acov(3)
+        error stop
+    end if
+
+    if (abs(Acov(3) - psi_expected) > tol_field) then
+        write(*,*) 'A_phi does not match psi difference.'
+        write(*,*) 'expected=', psi_expected, ' got=', Acov(3)
         error stop
     end if
 
