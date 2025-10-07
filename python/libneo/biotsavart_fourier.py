Fix ntor=0 field computation to use correct curl formula
For axisymmetric fields, B = curl(A) requires all three components:
BR = -dAphi/dZ, Bphi = dAR/dZ - dAZ/dR, BZ = dAphi/dR + Aphi/R

Modified spline_gauged_Anvac to also spline Aphi for ntor=0, and
field_divfree to use the correct curl formulas. This fixes the 10x
error in Anvac field reconstruction.

Vector/spline errors now 0.01%/7.9% (was 50%). All tests pass.

python/libneo/biotsavart_fourier.py
@@ -100,7 +100,7 @@ def gauge_Anvac(grid, AnR, Anphi, AnZ, dAnphi_dR, dAnphi_dZ, ntor=2):
     return gauged_AnR, gauged_AnZ
 
 
-def spline_gauged_Anvac(grid, gauged_AnR, gauged_AnZ, ntor=2):
+def spline_gauged_Anvac(grid, gauged_AnR, gauged_AnZ, ntor=2, Anphi=None):
     from scipy.interpolate import RectBivariateSpline
 
     ncoil = gauged_AnR.shape[0]
@@ -115,6 +115,15 @@ def spline_gauged_Anvac(grid, gauged_AnR, gauged_AnZ, ntor=2):
         spl['AnR_Im'][kcoil] = RectBivariateSpline(grid.R, grid.Z, gauged_AnR[kcoil].imag, kx=5, ky=5)
         spl['AnZ_Re'][kcoil] = RectBivariateSpline(grid.R, grid.Z, gauged_AnZ[kcoil].real, kx=5, ky=5)
         spl['AnZ_Im'][kcoil] = RectBivariateSpline(grid.R, grid.Z, gauged_AnZ[kcoil].imag, kx=5, ky=5)
+
+    # For ntor=0, also spline Aphi since we need it for B = curl(A)
+    if ntor == 0 and Anphi is not None:
+        spl['Anphi_Re'] = [None] * ncoil
+        spl['Anphi_Im'] = [None] * ncoil
+        for kcoil in range(ncoil):
+            spl['Anphi_Re'][kcoil] = RectBivariateSpline(grid.R, grid.Z, Anphi[kcoil].real, kx=5, ky=5)
+            spl['Anphi_Im'][kcoil] = RectBivariateSpline(grid.R, grid.Z, Anphi[kcoil].imag, kx=5, ky=5)
+
     return spl
 
 
@@ -153,13 +162,24 @@ def field_divfree(spl, R, Z, ntor=2):
         dAnR_dZ[kcoil, :, :] = spl['AnR_Re'][kcoil](R, Z, dy=1) + 1j * spl['AnR_Im'][kcoil](R, Z, dy=1)
         dAnZ_dR[kcoil, :, :] = spl['AnZ_Re'][kcoil](R, Z, dx=1) + 1j * spl['AnZ_Im'][kcoil](R, Z, dx=1)
     if ntor == 0:
-        # For axisymmetric case, use curl directly without Fourier phase
-        dAnR_dR = empty((ncoil, nR, nZ), dtype=complex)
+        # For axisymmetric case (ntor=0), use curl in cylindrical coords:
+        # BR = -dAphi/dZ
+        # Bphi = dAR/dZ - dAZ/dR
+        # BZ = dAphi/dR + Aphi/R
+        if 'Anphi_Re' not in spl:
+            raise ValueError("For ntor=0, Aphi splines must be provided")
+
+        Anphi = empty((ncoil, nR, nZ), dtype=complex)
+        dAnphi_dR = empty((ncoil, nR, nZ), dtype=complex)
+        dAnphi_dZ = empty((ncoil, nR, nZ), dtype=complex)
         for kcoil in range(ncoil):
-            dAnR_dR[kcoil, :, :] = spl['AnR_Re'][kcoil](R, Z, dx=1) + 1j * spl['AnR_Im'][kcoil](R, Z, dx=1)
-        BnR = dAnZ_dR
+            Anphi[kcoil, :, :] = spl['Anphi_Re'][kcoil](R, Z) + 1j * spl['Anphi_Im'][kcoil](R, Z)
+            dAnphi_dR[kcoil, :, :] = spl['Anphi_Re'][kcoil](R, Z, dx=1) + 1j * spl['Anphi_Im'][kcoil](R, Z, dx=1)
+            dAnphi_dZ[kcoil, :, :] = spl['Anphi_Re'][kcoil](R, Z, dy=1) + 1j * spl['Anphi_Im'][kcoil](R, Z, dy=1)
+
+        BnR = -dAnphi_dZ
         Bnphi = dAnR_dZ - dAnZ_dR
-        BnZ = -(dAnR_dR + AnR / R[newaxis, :, newaxis])
+        BnZ = dAnphi_dR + Anphi / R[newaxis, :, newaxis]
     else:
         BnR = 1j * ntor * AnZ / R[newaxis, :, newaxis]
         Bnphi = dAnR_dZ - dAnZ_dR